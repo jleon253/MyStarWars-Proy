b4da4307a81f972bb88827b84155630a821d7334
jleon253 <jleon253@gmail.com>
2020-04-24 16:13:01 -0500

    Reestructurando proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/organisms/login/login.component';
-import { HomeComponent } from './components/organisms/home/home.component';
 
 const routes: Routes = [
-  { path: 'login' , component: LoginComponent },
-  { path: 'home' , component: HomeComponent },
   { path: '**', redirectTo: 'login' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,28 +6,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/organisms/login/login.component';
-import { ButtonComponent } from './components/atoms/button/button.component';
-import { InputComponent } from './components/atoms/input/input.component';
-import { LabelComponent } from './components/atoms/label/label.component';
-import { InputFormComponent } from './components/molecules/input-form/input-form.component';
-import { CheckboxComponent } from './components/atoms/checkbox/checkbox.component';
-import { AlertComponent } from './components/molecules/alert/alert.component';
-import { HomeComponent } from './components/organisms/home/home.component';
-import { IdResourcePipe } from './pipes/id-resource.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    ButtonComponent,
-    InputComponent,
-    LabelComponent,
-    InputFormComponent,
-    CheckboxComponent,
-    AlertComponent,
-    HomeComponent,
-    IdResourcePipe
   ],
   imports: [
     BrowserModule,
