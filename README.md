d13474c20dd33c8ebc319edba0429b313908df26
jleon253 <jleon253@gmail.com>
2020-06-28 05:51:50 -0500

    Actualizando a Angular CLI 9.1.6
    Se actualizan los archivos de configuracion para el CLI
    
    se mantienen los archivos originales de las carpetas /src y de git
    
    Se agregan las antiguas dependencias de desarrollo y hojas de estilo CSS a los archivos package.json y angular.json

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# My Star Wars Repo
+# MyStarWarsProy
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.6.
 
 ## Development server
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/MyStarWars'),
+      dir: require('path').join(__dirname, './coverage/MyStarWars-Proy'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
